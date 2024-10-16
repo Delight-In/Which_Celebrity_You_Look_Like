05517cd351f0fbf526a718ea92e86d952a0d16bf
Delight-In <priyanka.professionalwork@gmail.com>
2024-10-15 22:44:57 -1200

    done

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Project',
-    author = 'Priyanka',
-    version = '0.0.1',
-    packages = find_packages(),
-    install_requirements = []
+    name='Project',
+    author='Priyanka',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    long_description='A brief description of your project.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
